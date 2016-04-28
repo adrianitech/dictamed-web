b742a09d4d96f8e1a4b4b1cd2bc030ade90174d3
Adrian Mateoaea <adrianitech@gmail.com>
2016-04-28 12:26:43 +0300

    Updates

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,6 +1,10 @@
 Template.home.helpers({
   'isEmpty': function(items) {
     return items.count() == 0;
+  },
+  'icon': function(title) {
+    if (title == 'Sent from Watch') { return 'mdi mdi-watch'; }
+    return 'mdi mdi-cellphone-iphone';
   }
 });
 
